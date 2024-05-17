232a43a7e39ea99280c764b24aa0d602681b1042
senchrus <rsenchakov@gmail.com>
2024-05-17 12:55:33 +0300

    tot

--- views/Report/_form_admin.php
+++ views/Report/_form_admin.php
@@ -18,7 +18,7 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'user_id')->textInput() ?>
 
-    <?= $form->field($model, 'status_id')->dropDownList([2 = 'Подверждено', 3 = 'Отклонено', ['promt'=>['text'=>'Новое', ['options'=>'display:none']]]]) ?>
+    <?= $form->field($model, 'status_id')->dropDownList([2 = 'Подверждено', 3 = 'Отклонено', ['promt'=>['text'=>'Новое', 'options'=>['style'=>'display:none']]]]) ?>
 
     <div class="form-group">
         <?= Html::submitButton('Сохранить', ['class' => 'btn btn-success']) ?>
